bb5d8e73f8bc7fef3eece08ed68474a1e5a4a1b9
jpusa <49599941+jpusa@users.noreply.github.com>
2020-03-03 17:41:34 +0200

    Back to old tests

--- tests/testthat/test-calibrate_yasso.R
+++ tests/testthat/test-calibrate_yasso.R
@@ -1,7 +1,12 @@
 test_that("Simulated carbon is correct for calibration", {
 
   # Expected output
-  real <- 220.986053559059
+  real_out <- matrix(data = c(33.31233, 22.00653, 15.3988, 11.24946, 3.50439,
+                              2.30436, 1.60405, 1.16489, 12.61624, 7.86392,
+                              5.04911, 3.2777, 20.79977, 24.73376, 26.20992,
+                              26.36682, 0.53731, 0.82382, 1.01263, 1.15024
+                              ),
+                     nrow = 4, ncol = 5)
 
   # Simulated output
   simulated_out <- calibrate_yasso(
@@ -15,8 +20,6 @@ test_that("Simulated carbon is correct for calibration", {
     leac = sample_data_cal$leac
   )
 
-  simulated <- sum(simulated_out)
-
   # Compare
-  expect_equal(simulated, real, tolerance = 1e-4)
+  expect_equal(simulated_out, real_out, tolerance = 1e-4)
 })
--- tests/testthat/test-run_yasso.R
+++ tests/testthat/test-run_yasso.R
@@ -1,7 +1,12 @@
 test_that("Simulated carbon is correct", {
 
   # Expected output
-  real <- 282.098786642746
+  real_out <- matrix(data = c(40.5, 33.31233, 15.24216, 6.83534, 3.93259, 30.5,
+                              3.50439, 1.58746, 0.69936, 0.39716, 20.3, 12.61624,
+                              4.98216, 1.44365, 0.45704, 10.3, 20.79977, 26.23076,
+                              24.64433, 19.35745, 0, 0.53731, 1.01746, 1.34135,
+                              1.56046),
+                     nrow = 5, ncol = 5)
 
   # Simulated output
   simulated_out <- run_yasso(
@@ -15,8 +20,6 @@ test_that("Simulated carbon is correct", {
     leac = sample_data_run$leac
   )
 
-  simulated <- sum(simulated_out)
-
   # Compare
-  expect_equal(simulated, real, tolerance = 1e-4)
+  expect_equal(simulated_out, real_out, tolerance = 1e-4)
 })
