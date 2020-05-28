522852d95cc5b4c23cc56e54c1a06bb5852ef5b8
jpusa <49599941+jpusa@users.noreply.github.com>
2020-05-28 13:57:30 +0300

    Added sample data of monthly temperature format

---
+++ data-raw/create_sample_data_run_Tmonthly.R
@@ -0,0 +1,35 @@
+# Amount of rows to run model over
+n_runs <- 4
+
+# Time for each run
+time <- c(1.01, 2.03, 3.01, 4.00)
+
+# Weather for each run
+weather_v <- c(9.710, 814.585, 7.3625)
+weather <- matrix(weather_v, nrow = n_runs, ncol = length(weather_v), byrow = TRUE)
+
+# Litter input for each run
+litter <- matrix(rep(0, len = n_runs * 4), nrow = n_runs)
+
+# Woody size for each run
+wsize <- rep(0, 4)
+
+# Leaching for each run
+leac <- rep(0, 4)
+
+# Initial carbon for each run
+init <- c(40.5, 30.5, 20.3, 10.3, 0)
+
+# Create a list, typeset for fortran
+sample_data_run_Tmonthly <- list(
+  "n_runs" = as.integer(n_runs),
+  "time" = as.double(time),
+  "temp" = as.matrix(temp),
+  "prec" = as.double(prec),
+  "init" = as.double(init),
+  "litter" = as.matrix(litter),
+  "wsize" = as.double(wsize),
+  "leac" = as.double(leac)
+)
+
+usethis::use_data(sample_data_run_Tmonthly, overwrite = TRUE)
